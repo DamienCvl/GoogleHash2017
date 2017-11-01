3f496563ef2eb800b13fffd33c1282d844a622a4
Damien CHEVALERIAS <damien.chevalerias@etu.univ-nantes.fr>
2017-11-01 12:16:01 +0100

    ajout des commentaires

--- Matrice.py
+++ Matrice.py
@@ -1,13 +1,13 @@
 class Matrice:
     
-    def __init__(self):
+    def __init__(self): # le tableau = une liste de liste
         self.matrice = [[]]
         
-    def setLine(self):
+    def setLine(self): # ajout d une liste pour representer une nouvelle ligne
         self.matrice.append([])
         
-    def setPoint(self,line,objet):
+    def setPoint(self,line,objet): # ajout d un objet a la suite de la 'line'
         self.matrice[line].append(objet)
         
-    def getPoint(self,x,y):
+    def getPoint(self,x,y): #getPoint(45,60) retournera l'objet de la ligne 45 colonne 60
         return self.matrice[x][y]
