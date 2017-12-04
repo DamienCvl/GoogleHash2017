049c3596666d5edddeb24ef209db98d956403ac9
Bastien CHUPIN <bastien.chupin@etu.univ-nantes.fr>
2017-12-04 20:07:52 +0100

    Update Void.py

--- Void.py
+++ Void.py
@@ -3,5 +3,5 @@ class Void:
     def __init__(self):
         pass
     #comportement d un Void a definir
-    def getType(self): #retourne le type, 0 pour Target, 1 pour Wall, 2 pour Void
+    def getType(self): #retourne le type, 0 pour Target, 1 pour Wall, 2 pour Void, 3 pour BackBone
         return 2
