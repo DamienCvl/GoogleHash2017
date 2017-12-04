8883fab16f52fb15ea770c876cb2aa6c85b19695
Bastien CHUPIN <bastien.chupin@etu.univ-nantes.fr>
2017-12-04 20:06:54 +0100

    Update Wall.py

--- Wall.py
+++ Wall.py
@@ -2,5 +2,5 @@ class Wall:
     def __init__(self):
         pass
     #comportement d un Wall a definir
-    def getType(self): #retourne le type, 0 pour Target, 1 pour Wall, 2 pour Void
+    def getType(self): #retourne le type, 0 pour Target, 1 pour Wall, 2 pour Void, 3 pour BackBone
         return 1
